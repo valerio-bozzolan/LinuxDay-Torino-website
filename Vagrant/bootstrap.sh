daf231c703a7610d67b08cfda3b7bd9abf4971f7
Valerio Bozzolan <valerio.bozzolan@gmail.com>
2017-10-02 03:19:57 +0200

    Vagrant provision twicks

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -5,50 +5,51 @@
 set -e
 
 PROJECT=/vagrant
-WWW=/var/www
+WWW="$PROJECT"
 DB_NAME=ldto
-DB_PREFIX=
+DB_PREFIX=ldto_
 BOZ_PHP=/usr/share/boz-php-another-php-framework
 
 # Very scaring
-DB_USER=root
-DB_PASSWORD=
+DB_USER=ldto
+DB_PASSWORD=ldto
 
 # Very scaring
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-apt-get install -y mariadb-server  \
-                php                \
-                php-mysql          \
-                apache2            \
-                libapache2-mod-php \
-                php-php-gettext    \
-                libmarkdown-php    \
-                libjs-jquery       \
-                libjs-leaflet      \
-		bzr
-
-a2enmod rewrite
-
-service apache2 restart
+apt-get install --yes mariadb-server     \
+                      php                \
+                      php-mysql          \
+                      apache2            \
+                      libapache2-mod-php \
+                      php-php-gettext    \
+                      libmarkdown-php    \
+                      libjs-jquery       \
+                      libjs-leaflet      \
+                      bzr
 
 if [ ! -e "$BOZ_PHP" ]; then
 	bzr branch lp:boz-php-another-php-framework "$BOZ_PHP"
-	chmod -R 750                                "$BOZ_PHP"
-	chown -R root:www-data                      "$BOZ_PHP"
+	chmod --recursive 750                       "$BOZ_PHP"
+	chown --recursive root:www-data             "$BOZ_PHP"
 fi
 
-if [ ! -h "$WWW" ]; then
-	rm -R             "$WWW"
-	ln -vs "$PROJECT" "$WWW"
-fi
+#if [ ! -h "$WWW" ]; then
+#	rm --recursive                     "$WWW"
+#	ln --symbolic --verbose "$PROJECT" "$WWW"
+#fi
 
-cp   "$PROJECT/htaccess.txt" "$WWW/.htaccess"
+cp "$PROJECT/htaccess.txt" "$WWW/.htaccess"
 
-echo "DROP DATABASE IF EXISTS \`$DB_NAME\`;" | mysql
-echo "CREATE DATABASE \`$DB_NAME\`;"         | mysql
-cat  "$PROJECT/database-schema.sql"          | mysql "$DB_NAME"
+mysql <<EOF
+DROP DATABASE IF EXISTS \`$DB_NAME\`;
+CREATE DATABASE \`$DB_NAME\`;
+GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO '$DB_USER'@localhost IDENTIFIED BY '$DB_PASSWORD';
+FLUSH PRIVILEGES;
+EOF
+
+cat "$PROJECT/database-schema.sql" | mysql --user=$DB_USER --password=$DB_PASSWORD "$DB_NAME"
 
 cat > "$WWW/load.php" <<EOF
 <?php
@@ -65,5 +66,33 @@ define('CONTACT_EMAIL', 'asd@asd.asd');
 define('CONTACT_PHONE', '555-555-555');
 require '$BOZ_PHP/load.php';
 EOF
+
+cat > /etc/apache2/sites-enabled/000-default.conf <<EOF
+<VirtualHost *:80>
+	ServerName  ldto
+	ServerAlias localhost 127.0.0.1
+
+	php_admin_flag  display_errors         1
+	php_admin_flag  display_startup_errors 1
+	php_admin_flag  html_errors            1
+	php_admin_value error_reporting       -1
+
+        LogLevel info
+
+	DocumentRoot $WWW
+
+	<Directory $PROJECT>
+		Options Indexes FollowSymLinks
+		AllowOverride All
+		Require all granted
+	</Directory>
+</VirtualHost>
+EOF
+
+a2enmod rewrite
+
+service apache2 restart
+
+chown --recursive root:www-data "$WWW/"
 
 echo "End provision."
