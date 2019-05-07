84dc00d1a97155658e6cb9ff26fa039590807bae
Valerio Bozzolan <gnu@linux.it>
2019-05-07 23:35:00 +0200

    start putting boz-php into 2019

---
+++ 2019/load.php
@@ -0,0 +1,39 @@
+<?php
+# Linux Day Torino website
+# Copyright (C) 2019 Valerio Bozzolan and contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ * This is the boz-php configuration file
+ */
+
+define('CURRENT_CONFERENCE_UID', '2019');
+
+// Boz-PHP: start
+require '../load.php';
+
+define('CURRENT_CONFERENCE_ROOT',                   ROOT . _  . CURRENT_CONFERENCE_UID);
+define('CURRENT_CONFERENCE_URL',                    URL  . _  . CURRENT_CONFERENCE_UID);
+define('CURRENT_CONFERENCE_ABSPATH', ABSPATH . __ . ROOT . __ . CURRENT_CONFERENCE_UID);
+
+// Autoload classes
+spl_autoload_register( function($c) {
+	$path = CURRENT_CONFERENCE_ABSPATH . "/include/class-$c.php";
+	if( is_file( $path ) ) {
+		require $path;
+	}
+} );
+
+require 'include/functions.php';
---
+++ 2019/template/footer.php
@@ -0,0 +1,63 @@
+<?php
+# Linux Day Torino website
+# Copyright (C) 2019 Ludovico Pavesi, Valerio Bozzolan and contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ * This is the footer of the website
+ */
+
+// do not visit directly
+defined( 'ABSPATH' ) or exit;
+
+?>
+<!-- =========================
+    FOOTER SECTION
+============================== -->
+<footer>
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-12 col-sm-12">
+				<p>Copyright &copy; 2019 Comitato Linux Day torino
+                    | Theme forked by <a rel="noreferrer nofollow" href="http://www.templatemo.com/page/1">Templatemo</a></p>
+
+				<ul class="social-icon">
+					<li><a href="https://www.facebook.com/LinuxDayTorino/" class="fa fa-facebook"></a></li>
+					<li><a href="https://twitter.com/linuxdaytorino" class="fa fa-twitter"></a></li>
+				</ul>
+
+			</div>
+		</div>
+	</div>
+</footer>
+
+
+<!-- Back top -->
+<a href="#back-top" class="go-top"><i class="fa fa-angle-up"></i></a>
+
+
+<!-- =========================
+     SCRIPTS
+============================== -->
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.parallax.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/smoothscroll.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
