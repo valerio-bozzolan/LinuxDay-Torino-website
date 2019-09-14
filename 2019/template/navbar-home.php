52573fe671ab0c7e92ae5f4043c8ce3f93b5e0f0
Valerio Bozzolan <gnu@linux.it>
2019-09-14 17:03:05 +0200

    add minor documentation

--- 2019/template/navbar-home.php
+++ 2019/template/navbar-home.php
@@ -1,3 +1,34 @@
+<?php
+# Linux Day Torino website
+# Copyright (C) 2019 Ludovico Pavesi, Valerio Bozzolan and contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ * This is the navbar of the website
+ */
+
+/*
+ * Args that should be passed:
+ *
+ * $conference (object) Current conference
+ */
+
+// do not visit directly
+defined( 'ABSPATH' ) or exit;
+
+?>
 <!-- =========================
      NAVIGATION LINKS
 ============================== -->
