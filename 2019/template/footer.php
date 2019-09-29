de32530f5140064a28a843672c2869d78fd9fffd
Valerio Bozzolan <gnu@linux.it>
2019-09-29 10:34:07 +0200

    change 2019 footer

--- 2019/template/footer.php
+++ 2019/template/footer.php
@@ -40,42 +40,44 @@ defined( 'ABSPATH' ) or exit;
 <footer>
 	<div class="container">
 		<div class="row">
-
 			<div class="col-md-12 col-sm-12">
 				<p><?= sprintf(
-					__( "I contenuti di questo sito sono liberamente fruibili in licenza di contenuto libero %s." ),
-					Licenses::instance()->get( 'cc-by-sa-4.0' )->getLink()
-				) ?></p>
-				<p><?= sprintf(
-					__( "Puoi clonare questo design con &hearts; scaricandolo da %s." ),
-					'<a href="http://www.templatemo.com/page/1">Templatemo</a>'
-				) ?></p>
-				<p><?= sprintf(
-					__( "Il motore del sito è rilasciato in licenza di software libero %s." ),
-					Licenses::instance()->get( 'gnu-agpl' )->getLink()
-				) ?></p>
-				<p><?= __( "Non si applicano politiche sulla protezione dei tuoi dati in quanto non ti abbiamo trasmesso cookie o altre tecnologie di tracciamento personale." ) ?></p>
+						__( "I contenuti di questo sito sono liberamente fruibili in licenza di contenuto libero %s." ),
+						Licenses::instance()->get( 'cc-by-sa-4.0' )->getLink()
+					) ?>
+					<br />
+					<?= sprintf(
+						__( "Puoi clonare questo design con &hearts; scaricandolo da %s." ),
+						'<a href="http://www.templatemo.com/page/1">Templatemo</a>'
+					) ?>
+					<br />
+					<?= sprintf(
+						__( "Il motore del sito è rilasciato in licenza di software libero %s." ),
+						Licenses::instance()->get( 'gnu-agpl' )->getLink()
+					) ?>
+					<br />
+					<?= __( "Non si applicano politiche sulla protezione dei tuoi dati in quanto non ti abbiamo trasmesso cookie o altre tecnologie di tracciamento personale." ) ?>
+				</p>
 
 				<div class="footer-hack">
-					<p><?= __( "Gli Hacker sono i benvenuti su questo sito!" ) ?></p>
 <pre>
+# Hackers are welcome in this website
 git clone <?= REPO ?>
 
 cd LinuxDay-Torino-website
 vagrant up
 </pre>
 				</div>
-
-				<ul class="social-icon">
-					<li><a href="https://www.facebook.com/LinuxDayTorino/" class="fa fa-facebook"></a></li>
-					<li><a href="https://twitter.com/linuxdaytorino" class="fa fa-twitter"></a></li>
-				</ul>
-
 			</div>
 		</div>
 		<div class="row" id="happy-hacking">
 			<div class="col-md-12 col-sm-12">
+				<hr />
 				<p><em>Share your Freedom!</em></p>
+				<ul class="social-icon">
+					<li><a href="https://www.facebook.com/LinuxDayTorino/" class="fa fa-facebook"></a></li>
+					<li><a href="https://twitter.com/linuxdaytorino" class="fa fa-twitter"></a></li>
+				</ul>
 			</div>
 		</div>
 	</div>
