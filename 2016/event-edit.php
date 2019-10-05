3d6f2f1c4763ce366a6e09362f8f589c0623cd72
Valerio Bozzolan <gnu@linux.it>
2019-10-05 10:57:59 +0200

    add an image to an amazing talk (mine asd)

--- 2016/event-edit.php
+++ 2016/event-edit.php
@@ -1,5 +1,5 @@
 <?php
-# Linux Day 2016 - single event profile page
+# Linux Day Torino website
 # Copyright (C) 2016, 2017, 2018, 2019 Valerio Bozzolan, Linux Day Torino
 #
 # This program is free software: you can redistribute it and/or modify
@@ -15,6 +15,13 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+/*
+ * Event edit
+ *
+ * From this page you can create/edit an User and assign some skills/interests etc.
+ */
+
+// load configurations and framework
 require 'load.php';
 
 // inherit the Conference or specify one
