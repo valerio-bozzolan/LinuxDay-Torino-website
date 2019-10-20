80d161ef96ae0f4de09704bd5f68074ad5bc40a7
Valerio Bozzolan <gnu@linux.it>
2019-10-20 12:28:00 +0200

    allow to use the pms strings from database

--- includes/functions.php
+++ includes/functions.php
@@ -35,11 +35,6 @@ function i18n_coalesce( $name, $i18n = null ) {
 	// current language (e.g. 'en')
 	$lang = latest_language()->getISO();
 
-	// TODO: add database fields for Piemontese
-	if( $lang === 'pms' ) {
-		$lang = 'it';
-	}
-
 	// default language of the website
 	$default = 'it';
 
